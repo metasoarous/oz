f5aa914548840e0eb25f72e233fd3aefd842fe2e
Soren Macbeth <soren@dopeness.org>
2015-10-28 19:17:20 -0700

    set groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vizard "0.1.0-SNAPSHOT"
+(defproject yieldbot/vizard "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
