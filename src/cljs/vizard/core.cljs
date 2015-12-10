a4fe5ec553644d20d4946abf4e458c7646b6291c
Soren Macbeth <soren@dopeness.org>
2015-12-10 13:35:52 -0800

    don't load an ugly default spec

--- src/cljs/vizard/core.cljs
+++ src/cljs/vizard/core.cljs
@@ -14,8 +14,8 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello world!"
-                          :spec {"width" 400, "height" 200, "padding" {"top" 10, "left" 30, "bottom" 20, "right" 10}, "marks" [{"type" "rect", "from" {"data" "table"}, "properties" {"enter" {"x" {"scale" "xscale", "field" "category"}, "width" {"scale" "xscale", "band" true, "offset" -1}, "y" {"scale" "yscale", "field" "amount"}, "y2" {"scale" "yscale", "value" 0}}, "update" {"fill" {"value" "steelblue"}}, "hover" {"fill" {"value" "red"}}}} {"type" "text", "properties" {"enter" {"align" {"value" "center"}, "fill" {"value" "#333"}}, "update" {"x" {"scale" "xscale", "signal" "tooltip.category"}, "dx" {"scale" "xscale", "band" true, "mult" 0.5}, "y" {"scale" "yscale", "signal" "tooltip.amount", "offset" -5}, "text" {"signal" "tooltip.amount"}, "fillOpacity" {"rule" [{"predicate" {"name" "tooltip", "id" {"value" nil}}, "value" 0} {"value" 1}]}}}}], "scales" [{"name" "xscale", "type" "ordinal", "range" "width", "domain" {"data" "table", "field" "category"}} {"name" "yscale", "range" "height", "nice" true, "domain" {"data" "table", "field" "amount"}}], "axes" [{"type" "x", "scale" "xscale"} {"type" "y", "scale" "yscale"}], "signals" [{"name" "tooltip", "init" {}, "streams" [{"type" "rect:mouseover", "expr" "datum"} {"type" "rect:mouseout", "expr" "{}"}]}], "predicates" [{"name" "tooltip", "type" "==", "operands" [{"signal" "tooltip._id"} {"arg" "id"}]}], "data" [{"name" "table", "values" [{"category" "A", "amount" 28} {"category" "B", "amount" 55} {"category" "C", "amount" 43} {"category" "D", "amount" 91} {"category" "E", "amount" 81} {"category" "F", "amount" 53} {"category" "G", "amount" 19} {"category" "H", "amount" 87} {"category" "I", "amount" 52}]}]}}))
+(defonce app-state (atom {:text "Hail Satan!"
+                          :spec {}}))
 
 (def packer (sente-transit/get-flexi-packer :edn))
 
