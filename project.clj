a5708d25951e0a63118ad81c5ae5f0a1092e71a3
Soren Macbeth <soren@yieldbot.com>
2017-03-25 17:32:40 -0700

    release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/vizard "0.2.0-SNAPSHOT"
+(defproject yieldbot/vizard "0.2.0"
   :description "Magic Visualization"
   :url "http://github.com/yieldbot/vizard"
   :license {:name "Eclipse Public License"
