80cea5e6f3050ef15accea2d71373f9ed585b12e
Christopher T Small <metasoarous@gmail.com>
2018-11-09 15:11:37 -0800

    finishing touches on cljsjs build script

--- bin/update-cljsjs.sh
+++ bin/update-cljsjs.sh
@@ -4,6 +4,7 @@
 set -euf -o pipefail
 
 
+
 if ! generate-extern -h > /dev/null
 then
   echo "You need to have generate-extern installed in order to run this script"
@@ -25,15 +26,19 @@ fi
 # For latest releases, see:
 # https://github.com/vega/vega/releases
 v_version="4.3.0"
+v_build_version="0"
 
 # https://github.com/vega/vega-lite/releases
 vl_version="3.0.0-rc8"
+vl_build_version="0"
 
 # https://github.com/vega/vega-embed/releases
 ve_version="3.23.1"
+ve_build_version="0"
 
 # https://github.com/vega/vega-tooltip/releases
 vt_version="0.13.0"
+vt_build_version="0"
 
 
 # store current directory
@@ -44,7 +49,7 @@ CLJSJS_PACKAGES_PATH="$oz_dir/cljsjs-packages"
 echo "Using cljsjs/packages path $CLJSJS_PACKAGES_PATH"
 
 
-# If the directory doesn't exists, clone
+# If the directory doesn't exist, clone
 if [ ! -d $CLJSJS_PACKAGES_PATH ]
 then
   git clone git@github.com:cljsjs/packages cljsjs-packages
@@ -58,13 +63,10 @@ cd $CLJSJS_PACKAGES_PATH
 git checkout master
 git pull origin master
 
-# Do our actual work on a special branch
-git checkout -B vega-updates
-
 
 
 # Vega dists env variable
-VEGA_DISTS="$oz_dir/vega-dists"
+VEGA_DISTS="$oz_dir/cljsjs-vega-dists"
 mkdir -p $VEGA_DISTS
 cd $VEGA_DISTS
 
@@ -93,12 +95,18 @@ extfile=vega-$v_version.ext.js
 generate-extern -f vega.js -n vega -o $extfile
 cp $extfile $CLJSJS_PACKAGES_PATH/vega/resources/cljsjs/vega/common/vega.ext.js
 
-
 # update lib versions and checksums in build.boot, and try installing
 cd $CLJSJS_PACKAGES_PATH/vega
 sed -i "s/def +lib-version+ \"[0-9a-z\.\-]*\"/def +lib-version+ \"$v_version\"/" build.boot
+sed -i "s/str +lib-version+ \"[0-9a-z\.\-]*\"/str +lib-version+ \"-$v_build_version\"/" build.boot
 sed -i "s/:checksum \"[0-9a-zA-Z]*\"/:checksum \"$v_checksum\"/" build.boot
 boot package install target
+
+# Commit to a dedicated branch
+git checkout -B vega-updates
+git add .
+git commit -m "[vega] update vega version to $v_version-$v_build_version"
+
 cd $VEGA_DISTS
 
 
@@ -124,9 +132,20 @@ cp $extfile $CLJSJS_PACKAGES_PATH/vega-lite/resources/cljsjs/vega-lite/common/ve
 
 # update lib versions and checksums in build.boot, and try installing
 cd $CLJSJS_PACKAGES_PATH/vega-lite
-sed -i "s/def +lib-version+ \"[0-9a-z\.\-]*\"/def +lib-version+ \"$vl_version\"/" build.boot
+sed -i "s/def +lib-version+ \"[0-9a-zA-Z\.\-]*\"/def +lib-version+ \"$vl_version\"/" build.boot
+sed -i "s/str +lib-version+ \"[0-9a-zA-Z\.\-]*\"/str +lib-version+ \"-$vl_build_version\"/" build.boot
 sed -i "s/:checksum \"[0-9a-zA-Z]*\"/:checksum \"$vl_checksum\"/" build.boot
+# update dependencies
+sed -i "s/\[cljsjs\/vega \"[0-9a-zA-Z\.\-]*\"\]/[cljsjs\/vega \"$v_version-$v_build_version\"]/" build.boot 
 boot package install target
+
+# Commit to a dedicated branch
+git checkout -B vega-lite-updates
+git add .
+git commit -m "[vega-lite] update vega-lite version to $vl_version-$vl_build_version" \
+           -m "Full version set: vega -> $v_version-$v_build_version, vega-lite -> $vl_version-$vl_build_version."
+
+# go back
 cd $VEGA_DISTS
 
 
@@ -156,11 +175,16 @@ cp $extfile $CLJSJS_PACKAGES_PATH/vega-embed/resources/cljsjs/vega-embed/common/
 # update lib versions and checksums in build.boot, and try installing
 cd $CLJSJS_PACKAGES_PATH/vega-embed
 sed -i "s/def +lib-version+ \"[0-9a-z\.\-]*\"/def +lib-version+ \"$ve_version\"/" build.boot
+sed -i "s/str +lib-version+ \"[0-9a-z\.\-]*\"/str +lib-version+ \"-$ve_build_version\"/" build.boot
 # note that this assumes the minified version comes second in the build process
 old_ve_checksum=$(grep -m 1 ":checksum" build.boot | grep -o "\"[a-zA-Z0-9]*\"")
 old_ve_min_checksum=$(grep -m 2 ":checksum" build.boot | tail -n 1 | grep -o "\"[a-zA-Z0-9]*\"")
+# update checksums
 sed -i "s/$old_ve_checksum/\"$ve_checksum\"/" build.boot
 sed -i "s/$old_ve_min_checksum/\"$ve_min_checksum\"/" build.boot
+# update dependencies
+sed -i "s/\[cljsjs\/vega \"[0-9a-zA-Z\.\-]*\"\]/[cljsjs\/vega \"$v_version-$v_build_version\"]/" build.boot 
+sed -i "s/\[cljsjs\/vega-lite \"[0-9a-zA-Z\.\-]*\"\]/[cljsjs\/vega-lite \"$vl_version-$vl_build_version\"]/" build.boot 
 boot package install target
 cd $VEGA_DISTS
 
@@ -194,17 +218,25 @@ cp $extfile $CLJSJS_PACKAGES_PATH/vega-tooltip/resources/cljsjs/vega-tooltip/com
 # update lib versions and checksums in build.boot, and try installing
 cd $CLJSJS_PACKAGES_PATH/vega-tooltip
 sed -i "s/def +lib-version+ \"[0-9a-z\.\-]*\"/def +lib-version+ \"$vt_version\"/" build.boot
+sed -i "s/str +lib-version+ \"[0-9a-z\.\-]*\"/str +lib-version+ \"-$vt_build_version\"/" build.boot
 # note that this assumes the minified version comes second in the build process, and css third
 old_vt_checksum=$(grep -m 1 ":checksum" build.boot | grep -o "\"[a-zA-Z0-9]*\"")
 old_vt_min_checksum=$(grep -m 2 ":checksum" build.boot | tail -n 1 | grep -o "\"[a-zA-Z0-9]*\"")
 old_vt_css_checksum=$(grep -m 3 ":checksum" build.boot | tail -n 1 | grep -o "\"[a-zA-Z0-9]*\"")
+# update checksums
 sed -i "s/$old_vt_checksum/\"$vt_checksum\"/" build.boot
 sed -i "s/$old_vt_min_checksum/\"$vt_min_checksum\"/" build.boot
 sed -i "s/$old_vt_css_checksum/\"$vt_css_checksum\"/" build.boot
+# update dependencies
+sed -i "s/\[cljsjs\/vega \"[0-9a-zA-Z\.\-]*\"\]/[cljsjs\/vega \"$v_version-$v_build_version\"]/" build.boot 
+sed -i "s/\[cljsjs\/vega-lite \"[0-9a-zA-Z\.\-]*\"\]/[cljsjs\/vega-lite \"$vl_version-$vl_build_version\"]/" build.boot 
 boot package install target
-cd $VEGA_DISTS
 
-
+# Commit vega-tooltip and vega-embed changes together as vega-extras
+git checkout -B vega-extras-updates
+git add .
+git commit -m "[vega-extras] update vega-embed & vega-tooltip versions" \
+           -m "Full version set: vega -> $v_version-$v_build_version, vega-lite -> $vl_version-$vl_build_version, vega-embed -> $ve_version-$ve_build_version, vega-tooltip -> $vt_version-$vt_build_version."
 
 
 
@@ -214,11 +246,15 @@ cd $VEGA_DISTS
 #echo "premature exit"
 #exit
 
-# Commit changes
+# If we have this fork env variable, we push to the fork
+if [[ ! -z ${CLJSJS_PACKAGES_FORK+x} ]]
+then
+  echo "Found CLJSJS_PACKAGES_PATH env variable; force pushing to branches: vega-updates, vega-lite-updates, and vega-extras-updates."
+  git push -f $CLJSJS_PACKAGES_FORK vega-updates:vega-updates
+  git push -f $CLJSJS_PACKAGES_FORK vega-lite-updates:vega-lite-updates
+  git push -f $CLJSJS_PACKAGES_FORK vega-extras-updates:vega-extras-updates
+fi
 
-cd $CLJSJS_PACKAGES_PATH
-git add vega vega-lite vega-tooltip vega-embed
-git commit -m "[vega] Update vega related packages" -m "Versions: vega -> $v_version, vega-lite -> $vl_version, vega-embed -> $ve_version, vega-tooltip -> $vt_version."
 
 # Let user know what they need to do next
 
@@ -228,10 +264,22 @@ echo "  ||  IMPORTANT!!! README!!!  ||"
 echo "  |============================|"
 echo ""
 echo "  Update almost complete!"
-echo "  A commit has been created with all the necessary changes."
+echo "  The cljsjs/packages repo at $CLJSJS_PACKAGES_PATH now has the following update branches: vega-updates, vega-lite-updates and vega-extras-updates."
+
+if [[ ! -z ${CLJSJS_PACKAGES_FORK+x} ]]
+then
+  echo "  These branches have now been pushed to your fork at: $CLJSJS_PACKAGES_FORK"
+else
+  echo ""
+  echo "  If you rerun with the CLJSJS_PACKAGES_FORK environment variable set to your fork of cljsjs/packages, this script will automatically push these branches there upon completion."
+  echo "  If you choose not to do this you will have to manually push these changes like."
+  echo ""
+  echo "    git push -f yourfork vega-updates:vega-updates"
+fi
+
 echo ""
-echo "  Next step is to 'cd cljsjs-packages', then run 'git show' to review the changes in this commit."
-echo "  Once you're satisfied, create a pull request to https://github.com/cljsjs/packages."
+echo "  The final step is to review the changes to make sure they look sane, then to create pull requests from each of these branches to https://github.com/cljsjs/packages."
+echo ""
 
 
 
