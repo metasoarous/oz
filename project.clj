dbd2390e36e61d2b6040b3b5d739541dee6a2c16
Christopher T Small <metasoarous@gmail.com>
2018-12-12 15:47:35 -0800

    beta1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metasoarous/oz "1.4.0-SNAPSHOT"
+(defproject metasoarous/oz "1.4.0-beta1-SNAPSHOT"
   :description "Great and powerful data visualizations in Clojure using Vega and Vega-lite"
   :url "http://github.com/metasoarous/oz"
   :license {:name "Eclipse Public License"
