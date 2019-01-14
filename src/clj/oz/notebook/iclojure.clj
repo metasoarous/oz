78a375b6cfdc9fdb814f4302d10391980f271da0
Christopher T Small <metasoarous@gmail.com>
2019-01-14 11:25:14 -0800

    Add shot at iclojure notebook integration

---
+++ src/clj/oz/notebook/iclojure.clj
@@ -0,0 +1,25 @@
+(ns oz.notebook.iclojure
+  "Experimental support for rendering vega in clojupyter"
+  (:require
+    [oz.core :as oz]
+    [lazy-require.core :as lreq]
+    [hiccup.core :as hiccup]
+    [cheshire.core :as json]))
+
+
+;; Something fancy that remembers if its been setup? Probablematic just to always run?
+;(defonce -setup?
+  ;(setup!))
+
+
+(defn view! [spec]
+  ;; problematic to always run?
+  (tagged-literal
+    'unrepl/mime
+    (if (map? spec)
+      {:content-type "application/vnd.vegalite.v2+json"
+       :content (json/generate-string spec)}
+      ;; otherwise assume hiccup, run embed code
+      {:content-type "text/html"
+       :content (hiccup/html (oz/embed spec))})))
+
