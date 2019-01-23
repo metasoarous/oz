463b8b57f5a9ce516669d83f5c918752d9a59c25
Christopher T Small <metasoarous@gmail.com>
2019-01-23 10:12:14 -0800

    patch version bump for documentation fixes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metasoarous/oz "1.5.1"
+(defproject metasoarous/oz "1.5.2"
   :description "Great and powerful data visualizations in Clojure using Vega and Vega-lite"
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
