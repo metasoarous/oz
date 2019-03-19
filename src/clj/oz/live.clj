4ff2fda9f6bf1ea043b3699eef493d6010b94527
Christopher T Small <metasoarous@gmail.com>
2019-03-19 15:06:41 -0700

    smooth out live reload

--- src/clj/oz/live.clj
+++ src/clj/oz/live.clj
@@ -1,9 +1,13 @@
 (ns ^:no-doc oz.live
   (:require [hawk.core :as hawk]
             [taoensso.timbre :as log]
-            [clojure.tools.reader.edn :as edn]))
+            [clojure.tools.reader :as reader]
+            [clojure.pprint :as pprint]
+            [clojure.core.async :as async]))
 
 
+(defn ppstr [x]
+  (with-out-str (pprint/pprint x)))
 
 (defonce watchers (atom {}))
 
@@ -41,30 +45,89 @@
          (dissoc reference-forms :refer-clojure)))}))
 
 
+(def ANSI_RED "\u001B[31m")
+(def ANSI_GREEN "\u001B[32m")
+(def ANSI_YELLOW "\u001B[33m")
+(def ANSI_BLUE "\u001B[34m")
+(def ANSI_RESET "\u001B[0m")
+
+(defn color-str
+  [color & ss]
+  (str color
+       (apply str ss)
+       ANSI_RESET))
+
+
 (defn reload-file! [filename context {:keys [kind file]}]
   ;; ignore delete (some editors technically delete the file on every save!
   (when (#{:modify :create} kind)
     (let [contents (slurp filename)
-          forms (edn/read-string (str "[" contents "]"))
-          last-forms (get-in @watchers [filename :last-forms])
-          diff-forms (->> (map vector
-                               (drop 1 forms)
-                               (concat (drop 1 last-forms)
-                                       (repeat nil)))
-                          (drop-while (fn [[f1 f2]] (= f1 f2)))
-                          (map first))
-          {:keys [ns-sym reference-forms]} (process-ns-form (first forms))]
-      ;; if there are differences, then do the thing
-      (when (seq diff-forms)
-        (log/info "Reloading file:" filename)
-        ;; Evaluate the ns form, and whatever forms thereafter differ from the last time we succesfully ran
-        (binding [*ns* (create-ns ns-sym)]
-          (eval
-            (concat '(do)
-                     reference-forms
-                     diff-forms)))
-        ;; Update last-forms in our state atom
-        (swap! watchers assoc-in [filename :last-forms] forms)))))
+          {:keys [last-contents last-forms]} (get @watchers filename)]
+      ;; This has a couple purposes, vs just using the (seq diff-forms) below:
+      ;; * forces at least a check if the file is changed at all before doing all the rest of the work below
+      ;;   (not sure how much perf benefit there is here)
+      ;; * there's sort of a bug and a feature: the (seq diff-forms) bit below ends up not working if
+      ;;   `last-forms` doesn't have everything, which will be the case if(f?) there was an error running
+      ;;   things. This means changing a ns form (e.g.) will trigger an update on forms that failed
+      ;; * this prevents the weird multiple callback issue with the way vim saves files creating 3/4 change
+      ;;   events
+      (when (not= contents last-contents)
+        (swap! watchers assoc-in [filename :last-contents] contents)
+        (let [forms (reader/read-string (str "[" contents "]"))
+              last-forms (get-in @watchers [filename :last-forms])
+              ;; gather all the forms that differ from the last run, and any that follow a differing form
+              ;; this may evolve into a full blown dependency graph thing
+              diff-forms (->> (map vector
+                                   (drop 1 forms)
+                                   (concat (drop 1 last-forms)
+                                           (repeat nil)))
+                              (drop-while (fn [[f1 f2]] (= f1 f2)))
+                              (map first))
+              {:keys [ns-sym reference-forms]} (process-ns-form (first forms))
+              successful-forms (atom [])]
+              ;; eventually add a final-form evaluation?
+              ;final-form (atom nil)]
+          ;; if there are differences, then do the thing
+          (when (seq diff-forms)
+            (log/info (color-str ANSI_GREEN "Reloading file:" filename))
+            ;; Evaluate the ns form's reference forms
+            (binding [*ns* (create-ns ns-sym)]
+              (eval (concat '(do) reference-forms)))
+            ;; Evaluate each of the following forms thereafter differ from the last time we successfully ran
+            (try
+              (doseq [form diff-forms]
+                (let [t0 (System/currentTimeMillis)
+                      result-chan (async/chan 1)
+                      timeout-chan (async/timeout 1000)
+                      long-running? (async/chan 1)]
+                  ;; Create a timeout on the result, and log a "in processing" message if necessary
+                  (async/go
+                    (let [[result chan] (async/alts! [result-chan timeout-chan])]
+                      (when (= chan timeout-chan)
+                        (log/info (color-str ANSI_YELLOW "Long running form being processed:\n" (ppstr form)))
+                        (async/>! long-running? true))))
+                  ;; actually run the code
+                  (try
+                    (binding [*ns* (create-ns ns-sym)]
+                      (let [result (eval form)]
+                        ;; put the result on the result chan, to let the go block above know we're done
+                        (async/>!! result-chan :done)
+                        ;; keep track of successfully run forms, so we don't redo work that completed
+                        (swap! successful-forms conj form)
+                        ;; If long running, log out how long it took
+                        (if (async/poll! long-running?)
+                          (println (color-str ANSI_YELLOW "Form processed in:" (/ (- (System/currentTimeMillis) t0) 1000.0) "s")))))
+                    (catch Exception e
+                      (log/error (color-str ANSI_RED "Error processing form:\n" (ppstr form)))
+                      (log/error e)
+                      (throw e)))))
+              (log/info (color-str ANSI_GREEN "Done reloading file:" filename "\n"))
+              (catch Exception e
+                (log/error (color-str ANSI_RED "Unable to process all of file:" filename "\n"))))
+            ;; Update last-forms in our state atom, only counting those forms which successfully ran
+            (let [base-forms (take (- (count forms) (count diff-forms)) forms)
+                  new-forms (concat base-forms @successful-forms)]
+              (swap! watchers assoc-in [filename :last-forms] new-forms))))))))
 
 
 (defn live-reload!
@@ -92,5 +155,6 @@
 
 (comment
   (live-reload! "dev/watchtest.clj")
+  (kill-watchers!)
   :end-comment)
 
