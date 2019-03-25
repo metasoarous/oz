881ece692b0120f48a823fe5c0303a6df244cb75
Christopher T Small <metasoarous@gmail.com>
2019-03-25 12:44:13 -0700

    update vega versions in export html

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metasoarous/oz "1.5.7-SNAPSHOT"
+(defproject metasoarous/oz "1.5.7-alpha1"
   :description "Great and powerful data visualizations in Clojure using Vega and Vega-lite"
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
