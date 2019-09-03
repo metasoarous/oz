87d28f8f604fcc96a0746ae0a38f702226214266
Christopher T Small <metasoarous@gmail.com>
2019-09-02 22:35:59 -0700

    release 1.6.0-alpha4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metasoarous/oz "1.6.0-alpha4-SNAPSHOT"
+(defproject metasoarous/oz "1.6.0-alpha4"
   :description "Great and powerful data visualizations in Clojure using Vega and Vega-lite"
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
