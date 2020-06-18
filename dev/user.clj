9eaf134e852c0988a9aaaee94ea2a3a5c000d529
Christopher Small <metasoarous@gmail.com>
2020-06-17 23:33:25 -0700

    remove figwheel related code from dev

--- dev/user.clj
+++ dev/user.clj
@@ -2,21 +2,13 @@
   (:require [oz.server :as server]
             [oz.core :as oz]
             [cheshire.core :as json]
-            [clojure.pprint :as pp]
-            [figwheel-sidecar.repl-api :as figwheel]))
+            [clojure.pprint :as pp]))
 
 ;; Let Clojure warn you when it needs to reflect on types, or when it does math
 ;; on unboxed numbers. In both cases you should add type annotations to prevent
 ;; degraded performance.
 ;(set! *warn-on-reflection* true)
 ;(set! *unchecked-math* :warn-on-boxed)
-
-(defn dev! []
-  (figwheel/start-figwheel!))
-
-(defn do-it-fools! []
-  (dev!)
-  (oz/start-server!))
 
 (def browser-repl figwheel/cljs-repl)
 
