2cf78b1005db1ffa5e6a0b18129d9184f74a769d
Christopher Small <metasoarous@gmail.com>
2020-06-28 16:53:21 -0700

    move examples to resources

--- examples/project/shadow-site/.shadow-cljs/builds/node-repl/dev/out/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,3 +0,0 @@
-
-
-cljs.core.apply.cljs$core$IFn$_invoke$arity$2(shadow.cljs.devtools.client.node_repl.main, process.argv.slice(2));
---
+++ resources/oz/examples/clj/clj_test.clj
@@ -0,0 +1,12 @@
+(ns clj-test)
+
+
+(def yo "yo")
+
+[:div
+ [:h1 "yo dawgsesseses"]]
+ ;[:vega-lite {:data {:values [{:a 1 :b 2} {:a 3 :b 1} {:a 8 :b 3}]}
+              ;:mark :point
+              ;:encoding {:x {:field :a}
+                         ;:y {:field :b}}}]]
+ 
