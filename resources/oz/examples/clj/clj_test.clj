d9cb43afff6453f037c58b6ca049e7bef3269ddb
Christopher Small <metasoarous@gmail.com>
2020-06-28 23:20:31 -0700

    clean up clj input example

--- resources/oz/examples/clj/clj_test.clj
+++ resources/oz/examples/clj/clj_test.clj
@@ -1,11 +1,8 @@
 (ns clj-test)
 
 
-(def yo "yo")
-
 [:div
- [:h1 "yo dawgses"]
- ;[:markdown "yo _dawgs_!"]]
+ [:h1 "yo dawgs"]
  [:vega-lite {:data {:values [{:a 1 :b 2} {:a 3 :b 1} {:a 8 :b 3}]}
               :mark :point
               :encoding {:x {:field :a}
