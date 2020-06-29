ff8e6eb36799b79d0ffce1fea637c38cb5478c90
Christopher Small <metasoarous@gmail.com>
2020-06-28 22:06:47 -0700

    add basic clj example workbook

--- resources/oz/examples/clj/clj_test.clj
+++ resources/oz/examples/clj/clj_test.clj
@@ -4,9 +4,10 @@
 (def yo "yo")
 
 [:div
- [:h1 "yo dawgsesseses"]]
- ;[:vega-lite {:data {:values [{:a 1 :b 2} {:a 3 :b 1} {:a 8 :b 3}]}
-              ;:mark :point
-              ;:encoding {:x {:field :a}
-                         ;:y {:field :b}}}]]
+ [:h1 "yo dawgses"]
+ ;[:markdown "yo _dawgs_!"]]
+ [:vega-lite {:data {:values [{:a 1 :b 2} {:a 3 :b 1} {:a 8 :b 3}]}
+              :mark :point
+              :encoding {:x {:field :a}
+                         :y {:field :b}}}]]
  
