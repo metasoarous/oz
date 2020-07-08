6c15e8502c2ea4470ce61f6d7a47bec1f93a5bad
Christopher Small <metasoarous@gmail.com>
2020-07-08 14:24:12 -0700

    more thorough catch and logging on failure to open browser window

--- src/clj/oz/server.clj
+++ src/clj/oz/server.clj
@@ -146,7 +146,11 @@
         (.browse (java.awt.Desktop/getDesktop) (java.net.URI. uri))
         (.exec (java.lang.Runtime/getRuntime) (str "xdg-open " uri)))
       (Thread/sleep 7500)
-      (catch java.awt.HeadlessException _))))
+      (catch Throwable t
+        (log/error "Unable to open a browser tab for you. Please visit" (str "http://localhost:" port))
+        (log/error t)))))
+
+
 (defn get-server-port [] (:port @web-server_))
 
 
