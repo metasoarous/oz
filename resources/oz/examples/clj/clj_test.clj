39f9e04116ea91a8d8feab836237b12ac25ba8ae
Christopher Small <metasoarous@gmail.com>
2020-08-28 22:51:41 -0700

    more vega example updates

--- resources/oz/examples/clj/clj_test.clj
+++ resources/oz/examples/clj/clj_test.clj
@@ -9,7 +9,7 @@
 
 [:vega-lite {:data {:values [{:a 1 :b 2} {:a 3 :b 1} {:a 8 :b 3}]}
              :mark :point
-             :encoding {:x {:field :a}
-                        :y {:field :b}}}]
+             :encoding {:x {:field :a :type :quantitative}
+                        :y {:field :b :type :quantitative}}}]
 
  
