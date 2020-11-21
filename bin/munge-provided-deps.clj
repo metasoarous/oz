5f2289338862cd1453ca939d4e476d172be87d86
Christopher Small <metasoarous@gmail.com>
2020-11-20 16:53:09 -0800

    fix munge-provided-deps script to use proper namespaces

--- bin/munge-provided-deps.clj
+++ bin/munge-provided-deps.clj
@@ -1,8 +1,18 @@
 #!/usr/bin/env bb
 
 (require '[clojure.data.xml :as xml]
-         '[clojure.string :as string])
+         '[clojure.string :as string]
+         '[clojure.walk :as walk]
+         '[clojure.pprint :as pp])
 
+(def pom-xml
+  (xml/parse (io/reader "pom.xml")))
+
+(def pom-ns (namespace (:tag (second (:content pom-xml)))))
+
+(defn nsify
+  [key]
+  (keyword pom-ns (name key)))
 
 (def clojupyter-version
   (-> (slurp "deps.edn")
@@ -15,15 +25,16 @@
 
 (def clojupyter-dep
   (xml/sexp-as-element
-    [:dependency
-     [:groupId "clojupyter"]
-     [:artifactId "clojupyter"]
-     [:version clojupyter-version]
-     [:scope "provided"]]))
+    [(nsify :dependency)
+     [(nsify :groupId) "clojupyter"]
+     [(nsify :artifactId) "clojupyter"]
+     [(nsify ::version) clojupyter-version]
+     [(nsify ::scope) "provided"]]))
 
 (def strip-emtpy-lines
   (partial remove
            (comp empty? string/trim)))
+
 
 (defn stringify-xml [xml]
   (->> xml
@@ -33,20 +44,32 @@
     (interpose "\n")
     (apply str)))
 
-(defn add-clojupyter-dep
-  [{:as xml :keys [content]}]
-  (assoc xml
-         :content
-         (map
-           (fn [{:as elmt :keys [tag content]}]
-             (if (and tag (= "dependencies" (name tag)))
-               (update elmt :content concat [clojupyter-dep])
-               elmt))
-           content)))
+(defn decorate-xml
+  [xml attrs]
+  (walk/postwalk
+    (fn [{:as elmt :keys [tag]}]
+      (if tag
+        (update elmt :attrs merge attrs)
+        elmt))
+    xml))
 
-(->> (xml/parse (io/reader "pom.xml"))
+(defn add-clojupyter-dep
+  [{:as xml :keys [content attrs]}]
+  (let [clojupyter-dep clojupyter-dep]
+  ;(let [clojupyter-dep (decorate-xml clojupyter-dep attrs)]
+    (assoc xml
+           :content
+           (map
+             (fn [{:as elmt :keys [tag content]}]
+               (if (and tag (= "dependencies" (name tag)))
+                 (update elmt :content concat [clojupyter-dep])
+                 elmt))
+             content))))
+
+(->> pom-xml
      (add-clojupyter-dep)
-     (stringify-xml)
+     (xml/emit-str)
+     ;(println))
      (spit "pom.xml"))
 
 
